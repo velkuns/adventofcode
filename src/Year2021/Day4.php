8c4b82faf3ad831d39e4d6e7b0d4aeed17f3ec32
velkuns <romain.inscription@hotmail.fr>
2021-12-04 17:18:30 +0100

    Add day 4

---
+++ src/Year2021/Day4.php
@@ -0,0 +1,196 @@
+<?php
+
+/*
+ * Copyright (c) Romain Cottard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+
+ */
+
+declare(strict_types=1);
+
+namespace Application\Year2021;
+
+use Application\Common\AlgorithmInterface;
+use Application\Pipeline\Pipeline;
+
+/**
+ * Class Day4
+ *
+ * @author Romain Cottard
+ */
+class Day4 implements AlgorithmInterface
+{
+    public function getExamples(string $star): array
+    {
+        $examples = [
+            '*'  => [
+                [4512 => [
+                    '7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1',
+                    '',
+                    '22 13 17 11  0',
+                    ' 8  2 23  4 24',
+                    '21  9 14 16  7',
+                    ' 6 10  3 18  5',
+                    ' 1 12 20 15 19',
+                    '',
+                    ' 3 15  0  2 22',
+                    ' 9 18 13 17  5',
+                    '19  8  7 25 23',
+                    '20 11 10 24  4',
+                    '14 21 16 12  6',
+                    '',
+                    '14 21 17 24  4',
+                    '10 16 15  9 19',
+                    '18  8 23 26 20',
+                    '22 11 13  6  5',
+                    ' 2  0 12  3  7',
+                ]],
+            ],
+            '**' => [
+                [4512 => [
+                    '7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1',
+                    '',
+                    '22 13 17 11  0',
+                    ' 8  2 23  4 24',
+                    '21  9 14 16  7',
+                    ' 6 10  3 18  5',
+                    ' 1 12 20 15 19',
+                    '',
+                    ' 3 15  0  2 22',
+                    ' 9 18 13 17  5',
+                    '19  8  7 25 23',
+                    '20 11 10 24  4',
+                    '14 21 16 12  6',
+                    '',
+                    '14 21 17 24  4',
+                    '10 16 15  9 19',
+                    '18  8 23 26 20',
+                    '22 11 13  6  5',
+                    ' 2  0 12  3  7',
+                ]],
+            ],
+        ];
+
+        return $examples[$star];
+    }
+
+    /**
+     * *  : Your puzzle answer was 3882564.
+     * ** : Your puzzle answer was 3385170.
+     */
+    public function solve(string $star, array $inputs, bool $functionalMode = false): string
+    {
+        if (!$functionalMode) {
+            return (string) ($star === '*' ? $this->starOne($inputs) : $this->starTwo($inputs));
+        } else {
+            return (string) ($star === '*' ? $this->starOneFunctional($inputs) : $this->starTwoFunctional($inputs));
+        }
+    }
+
+    private function starOne(array $inputs): int
+    {
+        $numbers = array_map('intval', explode(',', array_shift($inputs)));
+        $grids   = $this->parseGrids($inputs);
+
+        foreach ($numbers as $number) {
+            foreach ($grids as $grid) {
+                if ($grid->check($number)) {
+                    return $grid->solve($number);
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private function starTwo(array $inputs): int
+    {
+        return 0;
+    }
+
+    private function newGrid(array $lines): object
+    {
+        $grid = new class {
+            private array $lines   = [];
+            private array $columns = [];
+
+            public function set(array $lines): self
+            {
+                $this->lines = $lines;
+
+                return $this;
+            }
+
+            public function check(int $number): bool
+            {
+                foreach ($this->lines as &$line) {
+                    if (isset($line[$number])) {
+                        $line[$number] = 1;
+                        $isBingo = (array_sum($line) === 5);
+                        for ($i = 0, $max = count($line); $i < $max; $i++) {
+                            $isBingo = $isBingo || (array_sum(array_column($this->lines, $i)) === 5);
+                        }
+
+                        return $isBingo;
+                    }
+                }
+
+                return false;
+            }
+
+            public function solve(int $number): int
+            {
+                $sum = 0;
+                foreach ($this->lines as $line) {
+                    foreach ($line as $gridNumber => $check) {
+                        if ($check === 0) {
+                            $sum += $gridNumber;
+                        }
+                    }
+                }
+
+                return $sum * $number;
+            }
+        };
+
+        return $grid->set($lines);
+    }
+
+    private function parseGrids(array $inputs): array
+    {
+        $grids = [];
+        $lines = [];
+        foreach ($inputs as $line) {
+            if (empty($line)) {
+                if (!empty($lines)) {
+                    $grids[] = $this->newGrid($lines);
+                }
+                $lines = [];
+                continue;
+            }
+
+            $line    = array_map('intval', array_filter(explode(' ', $line), fn($number) => $number !== ''));
+            $lines[] = array_combine(array_values($line), array_fill(0, count($line), 0));
+        }
+
+        $grids[] = $this->newGrid($lines);
+
+        return $grids;
+    }
+
+
+    private function starOneFunctional(array $inputs): int
+    {
+        return 0;
+    }
+
+    private function starTwoFunctional(array $inputs): int
+    {
+        return 0;
+    }
+}
